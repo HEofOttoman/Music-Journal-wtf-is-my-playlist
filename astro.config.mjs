0d16952eb3e0e4673bfd99ed3d65af0ab66de43f
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-05-03 13:50:52 +1000

    tt

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Henry Wau`s Music Page',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
